a08e9746bf800d94e45272e8257d6fb9604f309d
unknown <nkundu191071@bscse.uiu.ac.bd>
2022-09-17 00:43:05 -0700

    Front-End (index, location & interface)

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include './header.php'; ?>
+
+
+<div class="p-4">
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+<?php include './footer.php'; ?>
---
+++ user/interface.php
@@ -0,0 +1,54 @@
+<?php include '../header.php'; ?>
+
+<div class="row justify-content-center">
+    <div class="col-md-8 p-4">
+    <div class="col-md-12 text-center">
+      <h3 class="events">Available Parking Systems</h3>
+    </div>
+        <div class="row">
+            <div class="col-sm-6 p-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Parking Name</h5>
+                        <p class="card-text">Available Slot: 6</p>
+                        <a href="#" class="btn btn-success">Want to book?</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 p-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Parking Name</h5>
+                        <p class="card-text">Available Slot: 6</p>
+                        <a href="#" class="btn btn-success">Want to book?</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 p-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Parking Name</h5>
+                        <p class="card-text">Available Slot: 6</p>
+                        <a href="#" class="btn btn-success">Want to book?</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 p-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Parking Name</h5>
+                        <p class="card-text">Available Slot: 6</p>
+                        <a href="#" class="btn btn-success">Want to book?</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include '../footer.php'; ?>
